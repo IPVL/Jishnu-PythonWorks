e6c2e4c634698ab4e44c934dec839c6f9af2496b
Jishnu <jbsu32@gmail.com>
2015-09-19 18:38:25 +0600

    finished chapter1

---
+++ chapter1/2.py
@@ -0,0 +1,25 @@
+#! /usr/bin/env python
+x=3
+print x
+print 3*3
+print x*x
+print x**x
+a = raw_input("a: ")
+b = raw_input("b: ")
+print int(a)*int(b)
+
+if 1==2: print "one equals two"
+if 1==1: print "one equals one"
+time = raw_input("input time: ")
+if int(time) % 60 == 0: print "an hour"
+print 2**3
+print pow (2,3)
+print pow (2.,3)
+print abs(-10)
+print round(1.0/2.0)
+print round(2.0/3.0)
+print round(6.0/4.5)
+print round(4/3)
+print round(7/2)
+print round(7./2)
+print round(7/4)
---
+++ chapter1/3.py
@@ -0,0 +1,45 @@
+#! /usr/bin/env python
+
+import math
+print math.floor(1.2)
+print math.floor(1.8)
+print int(math.floor(1.8))
+
+from math import sqrt
+print sqrt(32)
+print sqrt(16.9)
+print sqrt(16)
+print int(sqrt(16.9))
+
+import cmath
+print cmath.sqrt(-1)
+print 1j
+print cmath.sqrt(-5)
+print (3+3j)*(5+2j)
+
+name = raw_input("What is your name?: ")
+print "Hello "+name
+
+print "hello 'jishnu' "
+print 'hello "hello" '
+#print 'hello 'hello' '
+#print "hello "hello" "
+print 'hello \ hello'
+x = 'hello'
+y = 'world'
+print x+' '+y
+
+#i didn't understand the effect 
+#of using the repr function
+
+I1 = input     ("Enter a Number: ");
+print I1
+I2 = raw_input ("Enter a Number: ");
+print I2
+
+#i also don't understand the difference
+#between input and raw_input
+
+print 1+2+3\
+	+4+5 		#(Y)
+print "Hello \n world" #(Y)
